1a048b329aeeac0f4e42ac72cb56717203db57f9
Zach Routhier <zrouthier@gmail.com>
2014-09-30 15:42:56 -0600

    made help more explicit

--- lib/help.rb
+++ lib/help.rb
@@ -12,31 +12,31 @@ class Help
   end
 
   def load
-    "Load <filename> allows you to load a file."
+    "Load <filename> -allows you to load a file."
   end
 
   def queue_count
-    "Queue count tells you how many records are in the current queue."
+    "Queue count -tells you how many records are in the current queue."
   end
 
   def queue_clear
-    "Queue clear empties the current queue."
+    "Queue clear -empties the current queue."
   end
 
   def queue_print
-    "Queue print prints out a data table of the current queue."
+    "Queue print -prints out a data table of the current queue."
   end
 
   def queue_attribute
-    "Queue print by: <attribute> will print the data table sorted by a specific attribute such as zipcode."
+    "Queue print by: <attribute> -will print the data table sorted by a specific attribute such as zipcode."
   end
 
   def save_file
-    "Queue save to <filename.csv> will export the current queue to the specified filename as a CSV."
+    "Queue save to <filename.csv> -will export the current queue to the specified filename as a CSV."
   end
 
   def find_critera
-    "find <attribute> <criteria> will load the queue with all records matching the criteria for the given attribute."
+    "find <attribute> <criteria> -will load the queue with all records matching the criteria for the given attribute."
   end
 
 end
---
+++ test/entry_test.rb
@@ -0,0 +1,7 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class EntryTest < Minitest::Test
+
+  def test_
