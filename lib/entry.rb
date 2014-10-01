0e2af46520d33a3a7949080745e9f8bb77e563bc
Zach Routhier <zrouthier@gmail.com>
2014-10-01 15:16:52 -0600

    entry test is done

--- lib/entry.rb
+++ lib/entry.rb
@@ -1,18 +1,27 @@
 class Entry
-  attr_reader :id, :date, :first_name, :last_name, :email_address, :home_phone, :street, :city, :state, :zipcode
+  attr_reader :id,
+              :date,
+              :first_name,
+              :last_name,
+              :email_address,
+              :home_phone,
+              :street,
+              :city,
+              :state,
+              :zipcode
   #   ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode
 
   def initialize(data)
-    @id = data[0]
-    @date = clean_date(data[:regdate])
-    @first_name = clean_generic(data[:first_name])
-    @last_name = clean_generic(data[:last_name])
+    @id            = data[0]
+    @date          = clean_date(data[:regdate])
+    @first_name    = clean_generic(data[:first_name])
+    @last_name     = clean_generic(data[:last_name])
     @email_address = clean_generic(data[:email_address])
-    @home_phone = clean_phone_number(data[:homephone])
-    @street = clean_generic(data[:street])
-    @city = clean_generic(data[:city])
-    @state = clean_generic(data[:state])
-    @zipcode = clean_zipcode(data[:zipcode])
+    @home_phone    = clean_phone_number(data[:homephone])
+    @street        = clean_generic(data[:street])
+    @city          = clean_generic(data[:city])
+    @state         = clean_generic(data[:state])
+    @zipcode       = clean_zipcode(data[:zipcode])
   end
 
   def clean_generic(name)
